4a4ce79e7c1baf30de290c332e80d9b9cba107f9
ciocco97 <f.cremascoli@studenti.unibs.it>
2022-05-18 16:15:05 +0200

    Pagine di Login e Sign up + FirabeseAuth Da implementare il salvataggio dell'utente su DB

---
+++ app/src/main/java/com/eils/finalslappy/LoginActivity.kt
@@ -0,0 +1,89 @@
+package com.eils.finalslappy
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Button
+import com.eils.finalslappy.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    private lateinit var myAuth: FirebaseAuth
+
+    private val tag: String = "FinalSlappy"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Per rimuovere l'action bar ball'activity di login
+        supportActionBar?.hide()
+
+        // Ottenimento dell'istanza codivisa Firebase auth
+        myAuth = FirebaseAuth.getInstance()
+
+        binding.tvSignup.setOnClickListener {
+            Log.d("FinalSlappy", "Bottone cliccato")
+            intent = Intent(this, SignupActivity::class.java)
+            finish()
+            startActivity(intent)
+        }
+
+        binding.btnLogin.setOnClickListener {
+
+            val email = binding.etLoginEmail.text.toString().trim { it <= ' ' }
+            val password = binding.etLoginPassword.text.toString()
+
+            // Register error conditions
+            when {
+                TextUtils.isEmpty(email) -> {
+                    Log.w(tag, "Campo email vuoto")
+                }
+                TextUtils.isEmpty(password) -> {
+                    Log.w(tag, "Campo email vuoto")
+                }
+
+                else -> {
+                    firebaseAuthLogin(email, password)
+                }
+            }
+        }
+
+    }
+
+    private fun firebaseAuthLogin(email: String, password: String) {
+        myAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Login success
+                    Log.d(tag, "signInWithEmail:success")
+                    login()
+                } else {
+                    // Login in fails
+                    Log.w(tag, "signInWithEmail:failure", task.exception)
+                }
+            }
+    }
+
+    private fun login() {
+        intent = Intent(this, MainActivity::class.java)
+        finish()
+        startActivity(intent)
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = myAuth.currentUser
+        if(currentUser != null){
+            Log.w(tag, "Utente già loggato")
+        }
+    }
+
+}
---
+++ app/src/main/java/com/eils/finalslappy/SignupActivity.kt
@@ -0,0 +1,99 @@
+package com.eils.finalslappy
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import com.eils.finalslappy.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
+
+    private lateinit var myAuth: FirebaseAuth
+
+    private val tag: String = "FinalSlappy"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Per rimuovere l'action bar ball'activity di sign up
+        supportActionBar?.hide()
+
+        // Ottenimento dell'istanza codivisa Firebase auth
+        myAuth = FirebaseAuth.getInstance()
+
+        // Switch to login activity
+        binding.tvLogin.setOnClickListener {
+            intent = Intent(this, LoginActivity::class.java)
+            finish()
+            startActivity(intent)
+        }
+
+        binding.btnSignup.setOnClickListener {
+
+            val username = binding.etSignupUsername.text.toString().trim { it <= ' ' }
+            val email = binding.etSignupEmail.text.toString().trim { it <= ' ' }
+            val password = binding.etSignupPassword.text.toString()
+            val retiped_password = binding.etSignupRepeatPassword.text.toString()
+
+            // Register error conditions
+            when {
+                TextUtils.isEmpty(username) -> {
+                    Log.w(tag, "Campo username vuoto")
+                }
+                TextUtils.isEmpty(email) -> {
+                    Log.w(tag, "Campo email vuoto")
+                }
+                TextUtils.isEmpty(password) -> {
+                    Log.w(tag, "Campo email vuoto")
+                }
+                TextUtils.isEmpty(retiped_password) -> {
+                    Log.w(tag, "Campo email vuoto")
+                }
+                password != retiped_password -> {
+                    Log.w(tag, "Le due password non coincidono")
+                }
+
+                else -> {
+                    firebaseAuthSignup(email, password)
+                }
+            }
+        }
+
+    }
+
+    private fun firebaseAuthSignup(email: String, password: String) {
+        myAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign up success
+                    Log.d(tag, "createUserWithEmail:success")
+                    signup()
+                } else {
+                    // Sign up fails
+                    Log.w(tag, "createUserWithEmail:failure", task.exception)
+                }
+            }
+    }
+
+    private fun signup() {
+        intent = Intent(this, MainActivity::class.java)
+        finish()
+        startActivity(intent)
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = myAuth.currentUser
+        if(currentUser != null){
+            Log.w(tag, "Utente già loggato")
+        }
+    }
+
+}
